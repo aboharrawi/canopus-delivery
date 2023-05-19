864cf5b06c0f27b03bdf14aff89940cdc258cb6b
mharawi <mharawi@atypon.com>
2023-05-19 03:57:31 +0300

    Integrating with Eureka

--- src/main/java/com/canopus/delivery/cdn/StreamEndpoint.java
+++ src/main/java/com/canopus/delivery/cdn/StreamEndpoint.java
@@ -4,8 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.http.CacheControl;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,14 +16,14 @@ public class StreamEndpoint {
 
     private final Logger logger = LogManager.getLogger(StreamEndpoint.class);
 
-    @GetMapping("stream/{videoPath}")
-    public ResponseEntity<Resource> getVideo(@PathVariable("videoPath") String videoPath) {
+    @GetMapping("stream/{videoName}")
+    public ResponseEntity<Resource> getVideo(@PathVariable("videoName") String videoName) {
         UrlResource video;
         try {
-            logger.info("Resource fetch request with video path : " + videoPath);
-            video = new UrlResource("file:" + videoPath);
+            logger.info("Resource fetch request with video name : " + videoName);
+            video = new UrlResource("file:/var/lib/content/" + videoName);
             if (!video.exists()) {
-                logger.error("Video with path: " + videoPath + " was not found");
+                logger.error("Video with name: /var/lib/content/" + videoName + " was not found");
                 return ResponseEntity.notFound()
                         .build();
             }
@@ -34,8 +33,9 @@ public class StreamEndpoint {
                     .build();
         }
 
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.noStore())
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory.getMediaType(video).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .cacheControl(CacheControl.noCache())
                 .body(video);
     }
 }
